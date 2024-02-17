fe96f2ef71ffbb3cfe69c63ce67f1fd2661423dc
Bhargav Siva <bhargavsivacareer@gmail.com>
2024-02-17 16:07:03 -0700

    Part 3 Piece 1

--- App.js
+++ App.js
@@ -1,38 +1,31 @@
-
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", {id:"parent"}, [
-    React.createElement("div", {id:"child"}, [
-        React.createElement("h1",{},"Namasteee"),
-        React.createElement("h2",{},"H2 tag")
-    ]),
-    React.createElement("div", {id:"child2"}, [
-        React.createElement("h1",{},"H1 tag"),
-        React.createElement("h2",{},"H2 tag")
-    ])
-]);
+// React Element
+// const heading = React.createElement("h1", {id:"heading"}, "React fundamentals");
 
-// try writing style inside the object and other objects etc.
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading", xyz: "abc" },
-//   "Namaste Everyone"
-// );
-// const element = React.createElement("h1", {
-//     id : " First tag"
-// }, "Namaste");
+// JSX - HTML-like or XML-like syntax
+// transpiled before it reaches JS Engine - Done by Parcel
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root")
-); /*we are editing DOM, for editing we will use DOM*/
-// basically we are injecting React only into root, not other divs
-
-// passing a react element inside the root
-// root.render(heading);
-root.render(parent);
+// React element
+const jsxHeading = (
+  <h1 id="heading" className="head">
+    Heading using JSX
+  </h1>
+);
 
 // console.log(heading);
+// console.log(jsxHeading);
 
+// React Functional Component
+const HeadingComponent = () => {
+  return <h1>React Functional Component</h1>;
+};
 
+// const HeadingComponent2 = () => <h1>React Functional Component</h1>; ---> perfectly good.
+// no need return statement since it has only single statement, no return statement is required. ----> digest this syntax.
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(jsxHeading);
+root.render(<HeadingComponent />);
