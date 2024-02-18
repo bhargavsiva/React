9fdd1768e249ea6bbc58c1fc42bd8812ae686e67
Bhargav Siva <bhargavsivacareer@gmail.com>
2024-02-17 19:04:58 -0700

    Part 3 done

--- App.js
+++ App.js
@@ -1,31 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React Element
-// const heading = React.createElement("h1", {id:"heading"}, "React fundamentals");
+// const Title = () => (
+//   <h1 className="head" tabIndex="5">
+//     React JSX
+//   </h1>
+// );
 
-// JSX - HTML-like or XML-like syntax
-// transpiled before it reaches JS Engine - Done by Parcel
-
-// React element
-const jsxHeading = (
-  <h1 id="heading" className="head">
-    Heading using JSX
+const title = (
+  <h1 className="head" tabIndex="5">
+    React JSX
   </h1>
 );
 
-// console.log(heading);
-// console.log(jsxHeading);
+// Component composition - referencing a component inside another
+// const HeadingComponent = () => (
+//   <div id="container">
+//     <Title />
+//     <h1 className="heading">Namaste React Func Component</h1>
+//   </div>
+// );
 
-// React Functional Component
-const HeadingComponent = () => {
-  return <h1>React Functional Component</h1>;
-};
+const number = 10000;
 
-// const HeadingComponent2 = () => <h1>React Functional Component</h1>; ---> perfectly good.
-// no need return statement since it has only single statement, no return statement is required. ----> digest this syntax.
+// We can write JS code inside curly braces in the JSX code
+const HeadingComponent = () => (
+  <div id="container">
+    <h2>{100 + 200}</h2>
+    <h2>{title}</h2>
+    <h1 className="heading">Namaste React Func Component</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
 root.render(<HeadingComponent />);
