89add16f16c7d57d55cc5cb77845e58bd8acf7f0
Bhargav Siva <bhargavsivacareer@gmail.com>
2024-02-23 03:59:53 -0700

    Part 6 Piece 2

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,20 +6,27 @@ import Shimmer from "./Shimmer";
 const Body = () => {
   // State variable - super powerful.
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
+
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
     fetchData();
   }, []);
 
+  let json;
   const fetchData = async () => {
     const data = await fetch(
       "https://foodfire.onrender.com/api/restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING"
     ); //power of brower
 
-    const json = await data.json();
+    json = await data.json();
 
     // console.log(json);
     setListOfRestaurants(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredRestaurant(
       json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
   };
@@ -28,11 +35,33 @@ const Body = () => {
   //   if (listOfRestaurants.length === 0) {
   //     return <Shimmer />;
   //   }
-
+  //   console.log(searchText);
   return listOfRestaurants.length === 0 ? (
     <Shimmer />
   ) : (
     <div className="Body">
+      <div className="search">
+        <input
+          type="text"
+          className="search-box"
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            // Filter the rest cards and update the UI
+            // searchText
+            const filteredList = listOfRestaurants.filter((res) =>
+              res.info.name.toLowerCase().includes(searchText.toLowerCase())
+            );
+            setFilteredRestaurant(filteredList);
+          }}
+        >
+          Search
+        </button>
+      </div>
       <div className="fiter">
         <button
           className="filter-btn"
@@ -41,7 +70,7 @@ const Body = () => {
             const filteredList = listOfRestaurants.filter(
               (res) => res.info.avgRating > 4
             );
-            setListOfRestaurants(filteredList);
+            setFilteredRestaurant(filteredList);
             console.log(filteredList);
           }}
         >
@@ -49,7 +78,7 @@ const Body = () => {
         </button>
       </div>
       <div className="res-container">
-        {listOfRestaurants.map((restaurant) => (
+        {filteredRestaurant.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
